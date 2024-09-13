6d5455bab09eae62f799cf206e2e9e56e3ed70e0
YeoGeon Yoon <whaql12300@gmail.com>
2024-09-13 13:44:36 +0900

    first commit

---
+++ 1st_week/ocr_translate.py
@@ -0,0 +1,34 @@
+import easyocr
+import openai
+
+openai.api_key = "sk-HyP3YdBXiUJ2m6XGpjheo_wI1G_HkG_TMmZ0xFyy5bT3BlbkFJBOOqzPJ_HE3m8bMAtd8tfDz1N7uEblYllFDFKuOzAA"
+
+openai.api_key = "your openai api key"
+
+from_A = input('인식 및 번역할 언어 입력: ')
+to_B = input('번역될 언어 입력: ')
+
+reader = easyocr.Reader([from_A])
+result = reader.readtext('../image/example.png')
+
+translate_ = []
+for detection in result:
+    translate_.append(detection[1])
+
+translate_ = ' '.join(translate_)   
+
+completion = openai.ChatCompletion.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "system", 
+            "content": f"You're an AI for translation. When {from_A} comes in, translate it to {to_B}."
+        },
+        {
+            "role": "user",
+            "content": f'translate this {translate_} english to korea'
+        }
+    ]
+)
+
+print(completion.choices[0].message.content)
---
+++ README.md
@@ -0,0 +1,30 @@
+# OCR with ChatGPT automation(Translate)
+
+### 1. OpenAI API Key 입력
+
+![default](image/api_key.png)
+
+### 2. 코드 실행 및 언어 입력
+
+#### OCR 인식 이미지 경로
+
+![default](image/ocr_path.png)
+
+#### example 1
+- english -> korean
+
+![default](image/result1.png)
+
+- 결과 1
+
+![default](image/result2.png)
+
+#### example 2
+- english -> chinese
+
+![default](image/result3.png)
+
+- 결과 2
+
+![default](image/result4.png)
+
